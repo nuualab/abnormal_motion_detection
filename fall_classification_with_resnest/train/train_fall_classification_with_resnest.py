95332001d9ed9988526735f82f4f2942748c7272
younggeun-kim <eyfydsyd97@naver.com>
2020-12-21 16:19:48 +0900

    Update train_fall_classification_with_resnest.py
    rotate를 약화하고 blur처리등 다양한 agumentation들을 제거

--- fall_classification_with_resnest/train/train_fall_classification_with_resnest.py
+++ fall_classification_with_resnest/train/train_fall_classification_with_resnest.py
@@ -104,13 +104,13 @@ albumentations_transform = A.Compose([
     A.Resize(256, 256),
     A.RandomCrop(224, 224),
     A.Flip(p=0.5), # Same with transforms.RandomHorizontalFlip()
-    A.OneOf([A.MotionBlur(p=0.3),
-                         A.Blur(p=0.3),
-                          A.GaussNoise(p=0.3) ]),
-    A.Rotate(p=0.8),
+    #A.OneOf([A.MotionBlur(p=0.3),
+    #                     A.Blur(p=0.3),
+    #                      A.GaussNoise(p=0.3) ]),
+    A.Rotate(p=0.2),
     
-    A.RandomBrightnessContrast(p=0.2),
-    A.RGBShift(r_shift_limit=15, g_shift_limit=15, b_shift_limit=15, p=0.2),
+    #A.RandomBrightnessContrast(p=0.2),
+    #A.RGBShift(r_shift_limit=15, g_shift_limit=15, b_shift_limit=15, p=0.2),
 
     A.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),
     albumentations.pytorch.transforms.ToTensor()
