b1ed933572c888c50765a10f906f390d40b5e7b1
SJPark <cooingbliss@gmail.com>
2020-12-21 16:45:24 +0900

    Update predict_fall_classification_with_efficientnet.py
    efficientdet 모델에 Linear Layer 추가 및 validation에 쓰일 transform 추가

--- fall_classification_with_efficientnet/predict/predict_fall_classification_with_efficientnet.py
+++ fall_classification_with_efficientnet/predict/predict_fall_classification_with_efficientnet.py
@@ -27,3 +27,31 @@ weightdir = "./FallDown_efficientnetb4b_github/fallweight.pth"
 testfile = sorted(glob2.glob(inputdir+'/*'))
 
 net = ptcv_get_model('efficientnet_b4b', pretrained=True)
+
+class EfficientNet_model(nn.Module):
+    def __init__(self, net):
+        super(EfficientNet_model, self).__init__()        
+        self.backbone = net.features
+        self.drop = nn.Dropout(0.5)
+        self.out = nn.Linear(1792, 1)
+        
+    def forward(self, input):
+        x = self.backbone(input)
+        x = torch.flatten(x, 1)
+        x = self.drop(x)
+        output = self.out(x)
+        
+        return output
+
+Net = EfficientNet_model(net).cuda()
+Net.load_state_dict(torch.load(weightdir))
+Net.requires_grad_(False)
+Net.eval()
+
+val_transform = albumentations.Compose(
+    [
+        albumentations.Resize(224, 224),
+        albumentations.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),
+        albumentations.pytorch.transforms.ToTensor()
+    ]
+)
